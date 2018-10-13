87daf870b2216d0dcf9c0e4a5b2caf302c4114ac
Teddy Wing <git@teddywing.com>
2018-10-14 01:53:15 +0200

    New Xcode Cocoa static library project
    Initialised with Xcode version 9.2 (9C40b).

---
+++ dome_key_event_source_simulator/dome_key_event_source_simulator.h
@@ -0,0 +1,13 @@
+//
+//  dome_key_event_source_simulator.h
+//  dome_key_event_source_simulator
+//
+//  Created by tw on 10/14/18.
+//  Copyright © 2018 tw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface dome_key_event_source_simulator : NSObject
+
+@end
---
+++ dome_key_event_source_simulator/dome_key_event_source_simulator.m
@@ -0,0 +1,13 @@
+//
+//  dome_key_event_source_simulator.m
+//  dome_key_event_source_simulator
+//
+//  Created by tw on 10/14/18.
+//  Copyright © 2018 tw. All rights reserved.
+//
+
+#import "dome_key_event_source_simulator.h"
+
+@implementation dome_key_event_source_simulator
+
+@end
